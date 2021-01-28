379eea0c9a6340c43f6098d3258d91d49f5bf6fe
Nadji_Tan <nadjitan@yahoo.com.ph>
2021-01-28 22:30:38 +0800

    Fixes. Refactor, Brunei, Cambodia, & 100% navbar

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,19 +1,9 @@
-const Logo = document.querySelector("#logo");
-const MenuBurger = document.querySelector("#menu-burger");
-const MainBody = document.querySelector("#main-body");
-
 var blue = "#253680";
 var yellow = "#F9D552";
 var white = "#F5F5F5";
 
 var countryBallsAnimated = false;
 var mapTitleAnimated = false;
-
-var previousCountryInfoPage = document.getElementsByClassName(
-  "btn-overview"
-)[0]; //Change when new container-country opens
-
-var previousFolderTab = document.getElementsByClassName("folder-tab")[0];
 
 /* ----------- HOME PAGE EVENTS / ANIMATIONS ----------- */
 document
@@ -109,23 +99,6 @@ document
   });
 
 /* ----------- COUNTRY PAGE EVENTS / ANIMATIONS ----------- */
-document.querySelectorAll(".btn-back-map").forEach((item) => {
-  item.addEventListener("click", (event) => {
-    anime({
-      targets: "#container-countries",
-      translateX: [0, "100%"],
-      easing: "cubicBezier(.5, .05, .5, .5)",
-      duration: 600,
-      complete: function (anim) {
-        document.getElementById("container-countries").style.visibility =
-          "hidden";
-        document.querySelector("#group3").scrollIntoView({
-          behavior: "smooth",
-        });
-      },
-    });
-  });
-});
 
 addEventToBtn(".btn-overview", 0);
 addEventToBtn(".btn-food", 1);
@@ -149,11 +122,17 @@ function addEventToBtn(elName, elNum) {
 }
 
 function buttonStyle(btn) {
-  btn.style.cssText = "background-color: #f5f5f5; border: 1.5px solid #f5f5f5;";
+  if (previousCountryInfoPage == null) {
+    previousCountryInfoPage = btn.parentElement.getElementsByClassName(
+      "active-btn-navbar"
+    )[0];
+  }
+
+  btn.classList.add("active-btn-navbar");
 
   if (btn != previousCountryInfoPage) {
-    previousCountryInfoPage.style.cssText =
-      "border: 1.5px solid #253680; background-color: #f9d552;";
+    previousCountryInfoPage.classList.remove("active-btn-navbar");
+
     previousCountryInfoPage = btn;
   }
 }
@@ -181,6 +160,17 @@ function getContents(el, showContent) {
 
   foundElem.getElementsByClassName(contentDivs[showContent])[0].style.display =
     "grid";
+
+  // Fix SplideJS slides by refreshing them
+  splides.forEach((splide) => {
+    splide.refresh();
+  });
+  primarySplide.forEach((splide) => {
+    splide.refresh();
+  });
+  secondarySplide.forEach((splide) => {
+    splide.refresh();
+  });
 }
 
 /**
@@ -189,11 +179,25 @@ function getContents(el, showContent) {
  * Author: Nadji Tan
  *
  */
+
+document.getElementsByClassName("folder").forEach((folder) => {
+  folder.firstElementChild.firstElementChild.style.cssText =
+    "background-color: #f5f5f5; color: #253680;";
+});
+
 document.getElementsByClassName("folder").forEach((item) => {
+  var previousFolderTab;
+
   item.getElementsByClassName("folder-tab").forEach((tab, index) => {
     tab.addEventListener("click", (btn) => {
-      previousFolderTab.style.cssText =
-        "background-color: #253680; color: #f5f5f5;";
+      if (previousFolderTab != null) {
+        previousFolderTab.style.cssText =
+          "background-color: #253680; color: #f5f5f5;";
+      } else {
+        btn.target.parentElement.firstElementChild.style.cssText =
+          "background-color: #253680; color: #f5f5f5;";
+      }
+
       btn.target.style.cssText = "background-color: #f5f5f5; color: #253680;";
 
       item.getElementsByClassName("folder-content-body").forEach((body) => {
@@ -205,176 +209,5 @@ document.getElementsByClassName("folder").forEach((item) => {
 
       previousFolderTab = btn.target;
     });
-  });
-});
-
-/* ----------- MAPS ----------- */
-/**
- * AMCHARTS
- * Link: https://www.amcharts.com/
- *
- * Customized by: Nadji Tan
- *
- */
-am4core.ready(function () {
-  // Create map instance
-  var chart = am4core.create("chartdiv", am4maps.MapChart);
-
-  // Set map definition
-  chart.geodata = am4geodata_worldLow;
-  chart.background.fill = am4core.color(yellow);
-  chart.background.fillOpacity = 1;
-
-  // Set projection
-  chart.projection = new am4maps.projections.Miller();
-
-  // Center on the groups by default
-  chart.homeZoomLevel = 0;
-  chart.homeGeoPoint = { longitude: 117, latitude: 9.2 };
-
-  // Create map polygon series
-  var polygonSeries = chart.series.push(new am4maps.MapPolygonSeries());
-
-  // Make map load polygon (like country names) data from GeoJSON
-  polygonSeries.useGeodata = true;
-
-  // Configure series
-  var polygonTemplate = polygonSeries.mapPolygons.template;
-  polygonTemplate.tooltipHTML =
-    '<div style="text-align:center; color:#253680;"> <h3 style="font-weight:400; text-transform: uppercase;"> {name} </h3> <p style="font-style:italic; font-size:0.8rem;"> {subtext} </p> </div>';
-  polygonTemplate.stroke = am4core.color(yellow);
-
-  // Create hover state and set alternative fill color
-  var country = polygonTemplate.states.create("hover");
-  country.properties.fill = am4core.color(white);
-
-  var btnContainer = chart.createChild(am4core.Container);
-  btnContainer.shouldClone = false;
-  btnContainer.align = "right";
-  btnContainer.valign = "top";
-  //btnContainer.zIndex = Number.MAX_SAFE_INTEGER;
-  btnContainer.marginTop = 10;
-  btnContainer.marginRight = 20;
-
-  //var homeButton = btnContainer.createChild(am4core.Button);
-  var homeButton = new am4core.Button();
-  homeButton.events.on("hit", function () {
-    chart.goHome();
-  });
-
-  homeButton.icon = new am4core.Sprite();
-  homeButton.padding(7, 5, 7, 5);
-  homeButton.width = 30;
-  homeButton.scale = 1.5;
-  homeButton.cursorOverStyle = am4core.MouseCursorStyle.pointer;
-  homeButton.icon.path =
-    "M16,8 L14,8 L14,16 L10,16 L10,10 L6,10 L6,16 L2,16 L2,8 L0,8 L8,0 L16,8 Z M16,8";
-  homeButton.fill = blue;
-  homeButton.parent = chart;
-  homeButton.align = "right";
-  homeButton.valign = "bottom";
-  homeButton.marginBottom = 20;
-  homeButton.marginRight = 25;
-  //homeButton.insertBefore(chart.zoomControl.plusButton);
-
-  // Zoom control
-  chart.zoomControl = new am4maps.ZoomControl();
-  chart.zoomControl.parent = btnContainer;
-  chart.zoomControl.scale = 1.2;
-  chart.zoomControl.stroke = blue;
-  chart.zoomControl.slider.height = 70;
-  chart.zoomControl.slider.stroke = white;
-  chart.zoomControl.cursorOverStyle = am4core.MouseCursorStyle.pointer;
-
-  var excludedCountries = ["AQ"];
-
-  // Add some data
-  polygonSeries.data = [
-    {
-      title: "Brunei", // Custom data. Different from name
-      subtext: "A Kingdom of Unexpected Treasures",
-      fill: blue,
-      id: "BN", // With polygonSeries.useGeodata = true, it will try and match this id, then apply the other properties as custom data
-      //customData: "1995",
-    },
-    {
-      title: "Cambodia",
-      subtext: "Kingdom of Wonder",
-      fill: blue,
-      id: "KH",
-    },
-    {
-      title: "Indonesia",
-      subtext: "Wonderful Indonesia",
-      fill: blue,
-      id: "ID",
-    },
-    {
-      title: "Lao",
-      subtext: "Simply Beautiful",
-      fill: blue,
-      id: "LA",
-    },
-    {
-      title: "Malaysia",
-      subtext: "Truly Asia",
-      fill: blue,
-      id: "MY",
-    },
-    {
-      title: "Myanmar",
-      subtext: "Be Enchanted",
-      fill: blue,
-      id: "MM",
-    },
-    {
-      title: "Philippines",
-      subtext: "It's More Fun in the Philippines",
-      fill: blue,
-      id: "PH",
-    },
-    {
-      title: "Singapore",
-      subtext: "Passion Made Possible",
-      fill: blue,
-      id: "SG",
-    },
-    {
-      title: "Thailand",
-      subtext: "Amazing Thailand",
-      fill: blue,
-      id: "TH",
-    },
-    {
-      title: "Vietnam",
-      subtext: "Timeless Charm",
-      fill: blue,
-      id: "VN",
-    },
-  ];
-
-  var includedCountries = [];
-  polygonSeries.data.forEach(function (country) {
-    includedCountries.push(country.id);
-    excludedCountries.push(country.id);
-  });
-  polygonSeries.include = includedCountries;
-
-  // // The rest of the world.
-  // var worldSeries = chart.series.push(new am4maps.MapPolygonSeries());
-  // var worldSeriesName = "world";
-  // worldSeries.name = worldSeriesName;
-  // worldSeries.useGeodata = true;
-  // worldSeries.exclude = excludedCountries;
-  // worldSeries.fillOpacity = 1;
-  // worldSeries.hiddenInLegend = true;
-  // worldSeries.mapPolygons.template.nonScalingStroke = true;
-
-  // Bind "fill" property to "fill" key in data
-  polygonTemplate.propertyFields.fill = "fill";
-
-  polygonTemplate.events.on("hit", function (ev) {
-    ev.target.series.chart.zoomToMapObject(ev.target);
-    console.log(ev.target.dataItem.dataContext.name);
   });
 });
---
+++ dist/js/charts.js
@@ -0,0 +1,254 @@
+Chart.pluginService.register({
+  beforeDraw: function (chart) {
+    if (chart.config.options.elements.center) {
+      // Get ctx from string
+      var ctx = chart.chart.ctx;
+
+      // Get options from the center object in options
+      var centerConfig = chart.config.options.elements.center;
+      var fontStyle = centerConfig.fontStyle || "Arial";
+      var txt = centerConfig.text;
+      var color = centerConfig.color || "#000";
+      var maxFontSize = centerConfig.maxFontSize || 75;
+      var sidePadding = centerConfig.sidePadding || 20;
+      var sidePaddingCalculated = (sidePadding / 100) * (chart.innerRadius * 2);
+      // Start with a base font of 30px
+      ctx.font = "30px " + fontStyle;
+
+      // Get the width of the string and also the width of the element minus 10 to give it 5px side padding
+      var stringWidth = ctx.measureText(txt).width;
+      var elementWidth = chart.innerRadius * 2 - sidePaddingCalculated;
+
+      // Find out how much the font can grow in width.
+      var widthRatio = elementWidth / stringWidth;
+      var newFontSize = Math.floor(30 * widthRatio);
+      var elementHeight = chart.innerRadius * 2;
+
+      // Pick a new font size so it will not be larger than the height of label.
+      var fontSizeToUse = Math.min(newFontSize, elementHeight, maxFontSize);
+      var minFontSize = centerConfig.minFontSize;
+      var lineHeight = centerConfig.lineHeight || 25;
+      var wrapText = false;
+
+      if (minFontSize === undefined) {
+        minFontSize = 20;
+      }
+
+      if (minFontSize && fontSizeToUse < minFontSize) {
+        fontSizeToUse = minFontSize;
+        wrapText = true;
+      }
+
+      // Set font settings to draw it correctly.
+      ctx.textAlign = "center";
+      ctx.textBaseline = "middle";
+      var centerX = (chart.chartArea.left + chart.chartArea.right) / 2;
+      var centerY = (chart.chartArea.top + chart.chartArea.bottom) / 2;
+      ctx.font = fontSizeToUse + "px " + fontStyle;
+      ctx.fillStyle = color;
+
+      if (!wrapText) {
+        ctx.fillText(txt, centerX, centerY);
+        return;
+      }
+
+      var words = txt.split(" ");
+      var line = "";
+      var lines = [];
+
+      // Break words up into multiple lines if necessary
+      for (var n = 0; n < words.length; n++) {
+        var testLine = line + words[n] + " ";
+        var metrics = ctx.measureText(testLine);
+        var testWidth = metrics.width;
+        if (testWidth > elementWidth && n > 0) {
+          lines.push(line);
+          line = words[n] + " ";
+        } else {
+          line = testLine;
+        }
+      }
+
+      // Move the center up depending on line height and number of lines
+      centerY -= (lines.length / 2) * lineHeight;
+
+      for (var n = 0; n < lines.length; n++) {
+        ctx.fillText(lines[n], centerX, centerY);
+        centerY += lineHeight;
+      }
+      //Draw text in center
+      ctx.fillText(line, centerX, centerY);
+    }
+  },
+});
+
+var options;
+function editOptions(text, color, legend) {
+  options = {
+    elements: {
+      center: {
+        text: text,
+        color: color,
+        fontStyle: "Roboto",
+        sidePadding: 60,
+        minFontSize: 5,
+        lineHeight: 25,
+      },
+    },
+    responsive: false,
+    cutoutPercentage: 70,
+    legend: legend,
+    tooltips: {
+      enabled: true,
+      xPadding: 40,
+      yPadding: 20,
+      backgroundColor: "rgba(0, 0, 0, 0.438)",
+      caretSize: 10,
+      caretPadding: 5,
+      titleFontFamily: "Roboto",
+      titleFontStyle: "normal",
+      titleFontSize: 20,
+      titleFontColor: "#f5f5f5",
+      titleAlign: "center",
+      titleMarginBottom: 0,
+      height: 5,
+      displayColors: false,
+      callbacks: {
+        title: function (tooltipItem, data) {
+          return (
+            data["datasets"][0]["data"][tooltipItem[0]["index"]] +
+            "% " +
+            data["labels"][tooltipItem[0]["index"]]
+          );
+        },
+        label: function (tooltipItem, data) {
+          return null;
+          // return (
+          //   data["datasets"][0]["data"][tooltipItem["index"]] +
+          //   "% " +
+          //   data["labels"][tooltipItem["index"]]
+          // );
+        },
+      },
+    },
+  };
+}
+
+var ctx1;
+var doughnutChart;
+function makeChart(elemId, data) {
+  ctx1 = document.getElementById(elemId).getContext("2d");
+
+  doughnutChart = new Chart(ctx1, {
+    type: "doughnut",
+    data: data,
+    options: options,
+  });
+}
+
+/*---------- BRUNEI ---------- */
+// Religion Doughnut Chart
+editOptions("RELIGION", "#253680", {
+  display: true,
+  position: "left",
+  labels: {
+    boxWidth: 30,
+    padding: 25,
+    fontFamily: "Roboto",
+  },
+});
+makeChart("doughnut-chart-religion-brunei", {
+  labels: [
+    "Christian",
+    "Muslim (Sunni Muslim)",
+    "Buddhist",
+    "Traditional Beliefs",
+  ],
+  datasets: [
+    {
+      label: "Brunei Religions",
+      data: [8.7, 78.8, 7.8, 4.7],
+      backgroundColor: ["#253680", "#f5f5f5", "#253680", "#253680"],
+      borderColor: ["#f9d552", "#f9d552", "#f9d552", "#f9d552"],
+      borderWidth: 6,
+      hoverBorderWidth: 2,
+    },
+  ],
+});
+
+// Language Doughnut Chart
+editOptions("LANGUAGE", "#f9d552", {
+  display: true,
+  position: "bottom",
+  labels: {
+    boxWidth: 30,
+    padding: 25,
+    fontFamily: "Roboto",
+    fontColor: "#f5f5f5",
+  },
+});
+makeChart("doughnut-chart-language-brunei", {
+  labels: ["Malays", "Chinese", "Others"],
+  datasets: [
+    {
+      label: "Brunei Languages",
+      data: [65.8, 10.2, 24.0],
+      backgroundColor: ["#f5f5f5", "#f9d552", "#f9d552"],
+      borderColor: ["#253680", "#253680", "#253680"],
+      borderWidth: 6,
+      hoverBorderWidth: 2,
+    },
+  ],
+});
+/*---------- END OF BRUNEI ---------- */
+
+/*---------- CAMBODIA ---------- */
+// Religion Doughnut Chart
+editOptions("RELIGION", "#253680", {
+  display: true,
+  position: "left",
+  labels: {
+    boxWidth: 30,
+    padding: 25,
+    fontFamily: "Roboto",
+  },
+});
+makeChart("doughnut-chart-religion-cambodia", {
+  labels: ["Muslim", "Other Cambodia Religions", "Buddhism", "Christianity"],
+  datasets: [
+    {
+      label: "Cambodia Religions",
+      data: [2.3, 6.4, 90, 1.3],
+      backgroundColor: ["#253680", "#253680", "#f5f5f5", "#253680"],
+      borderColor: ["#f9d552", "#f9d552", "#f9d552", "#f9d552"],
+      borderWidth: 6,
+      hoverBorderWidth: 2,
+    },
+  ],
+});
+
+// Language Doughnut Chart
+editOptions("LANGUAGE", "#f9d552", {
+  display: true,
+  position: "bottom",
+  labels: {
+    boxWidth: 30,
+    padding: 25,
+    fontFamily: "Roboto",
+    fontColor: "#f5f5f5",
+  },
+});
+makeChart("doughnut-chart-language-cambodia", {
+  labels: ["N/A"],
+  datasets: [
+    {
+      label: "Cambodia Languages",
+      data: [100],
+      backgroundColor: ["#f9d552"],
+      borderColor: ["#253680"],
+      borderWidth: 6,
+      hoverBorderWidth: 2,
+    },
+  ],
+});
+/*---------- END OF CAMBODIA ---------- */
